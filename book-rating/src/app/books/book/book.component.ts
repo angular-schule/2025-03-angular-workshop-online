26e9c5ba261d3d4f114ed8d33aa7ddf8b077ab25
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-26 21:29:54 +0100

    Pipes

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,9 +1,11 @@
 import { Component, input, output } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-book',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
