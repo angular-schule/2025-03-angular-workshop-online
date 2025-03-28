e3df06a8b139d014ccca52b5b4a9a64c890e57d1
Ferdinand Malcher <ferdinand@malcher.media>
2025-03-28 10:51:05 +0100

    Vorbereitung Formulare und Suche

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { booksRoutes } from './books/books.routes';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
   // bei Weiterleitung von leerem Pfad (fast) immer pathMatch:full nötig
-  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   ...booksRoutes,
   // Wildcard-Route: immer ganz unten!
   { path: '**', component: NotFoundComponent, title: 'Not Found' },
--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { BookDetailsComponent } from "./book-details/book-details.component";
+import { BookSearchComponent } from "./book-search/book-search.component";
+import { BookCreateComponent } from "./book-create/book-create.component";
 
 export const booksRoutes: Routes = [
   { path: 'books', component: DashboardComponent, title: 'Dashboard' },
-  { path: 'books/:isbn', component: BookDetailsComponent, title: 'Details' }
+  { path: 'books/search', component: BookSearchComponent, title: 'Suche' },
+  { path: 'books/create', component: BookCreateComponent, title: 'Buch erstellen' },
+  { path: 'books/:isbn', component: BookDetailsComponent, title: 'Details' },
 ];
