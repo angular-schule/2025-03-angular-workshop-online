623753a7ab64cf720be9d2f6b11346c7de744376
Ferdinand Malcher <ferdinand@malcher.media>
2025-04-01 08:27:56 +0200

    Hausaufgabe: Datum anzeigen

--- book-rating/src/app/home-page/home-page.component.ts
+++ book-rating/src/app/home-page/home-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnDestroy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnDestroy {
+  readonly currentDate = signal(Date.now());
 
+  #interval = setInterval(() => {
+    this.currentDate.set(Date.now());
+    console.log(this.currentDate());
+  }, 1000);
+
+  ngOnDestroy() {
+    clearInterval(this.#interval);
+  }
 }
